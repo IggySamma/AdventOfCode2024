1dd442aa6bc71ed2af6c0a9903217ca9050e9cb2
IggySamma <ignasfb@yahoo.ie>
2024-12-03 20:42:31 +0000

    Day2 Part 1 Solved

--- Day2/Day2.go
+++ Day2/Day2.go
@@ -34,43 +34,31 @@ func main() {
 			line = append(line, temp)
 		}
 
-		var direction []int64
-
-		err := checkSafe(line, direction)
-		if err != nil {
-			direction = nil
-			break
+		check := checkSafe(line)
+		if check == nil {
+			count++
 		}
 
-		//err := checkSafeAdded()
 	}
 
 	fmt.Println(count)
 }
 
-func checkSafeAdded(x []int64, count int) error {
-	for i := 1; i < len(x); i++ {
-		if x[i-1] != x[i] {
-			x = nil
-			return errors.New("1")
-		}
-	}
-	count++
-	return nil
-}
-
-func checkSafe(line []int64, direction []int64) error {
-	for i := 0; i < len(line)-1; i++ {
-		if ((line[i+1] - line[i]) < 4) && ((line[i+1] - line[i]) > -4) {
-			if (line[i+1] - line[i]) > 0 {
-				direction = append(direction, 1)
-			} else if (line[i+1] - line[i]) < 0 {
-				direction = append(direction, 0)
+func checkSafe(line []int64) error {
+	if (line[1] - line[0]) > 0 {
+		for i := 0; i < len(line)-1; i++ {
+			if !((line[i+1]-line[i]) > 0 && (line[i+1]-line[i]) < 4) {
+				return errors.New("1")
 			}
-		} else {
-			return errors.New("1")
 		}
-
+	} else if (line[1] - line[0]) < 0 {
+		for i := 0; i < len(line)-1; i++ {
+			if !((line[i+1]-line[i]) < 0 && (line[i+1]-line[i]) > -4) {
+				return errors.New("1")
+			}
+		}
+	} else {
+		return errors.New("1")
 	}
 	return nil
 }
